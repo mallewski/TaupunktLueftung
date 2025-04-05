da4a1cc8ac171f0aea8af4cc1d457f8085ebde58
mallewski <m.ellekotten@posteo.de>
2025-04-05 08:16:46 +0200

    Projekt-Link added

--- TaupunktLueftung.ino
+++ TaupunktLueftung.ino
@@ -827,10 +827,10 @@ void handleRoot() {
   html += "<link rel='stylesheet' href='/style.css'>";
 
   html += "</head><body>";
-  html += "<h1>" + String(NAME) + " Interface</h1>";
+  html += "<h1>" + String(NAME) + " " + String(FIRMWARE_VERSION) + " Interface</h1>";
 
   // Tabs oben
- html += "<p><button onclick=\"showTab('dashboard')\">Dashboard</button>";
+  html += "<p><button onclick=\"showTab('dashboard')\">Dashboard</button>";
   html += "<button onclick=\"localStorage.setItem('manualSettingsClick','true'); showTab('settings')\">Einstellungen</button>";
 
   // Eingefügte UI-Blöcke:
@@ -966,10 +966,27 @@ String getSettingsHtml() {
 
   // Firmware-Button
   html += "<fieldset><legend>Firmware</legend>";
-  //html += "<button type='button' id='firmwareUpdateBtn' onclick='openFirmwareModalUI()'>Firmware-Update</button>";
-  //html += "<p><button type='button' id='firmwareUpdateBtn' onclick=\"localStorage.setItem('manualSettingsClick','true'); showTab('settings')\">Firmware-Update</button></p>";
   html += "<p><button type='button' onclick=\"showTab('settings'); setTimeout(openFirmwareModalUI, 100);\">Firmware-Update</button></p>";
   html += "</fieldset>";
+
+  html += "<p align='center'>";
+  html += "<a href='https://github.com/mallewski/TaupunktLueftung' target='_blank' "
+        "style='display:inline-block;text-decoration:none;padding:6px 12px;"
+        "background:#24292e;color:white;border-radius:5px;font-weight:bold;'>"
+        "<svg height='16' width='16' viewBox='0 0 16 16' fill='white' "
+        "style='vertical-align:middle;margin-right:6px;'>"
+        "<path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38"
+        " 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13"
+        "-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07"
+        "-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08"
+        "-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27"
+        "c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15"
+        " 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2"
+        " 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z'/>"
+        "</svg> Projekt auf GitHub</a>";
+  html += "</p>";
+  html += "<p align='center'>Wenn du dieses Projekt nützlich findest, kannst du mir einen Kaffee spendieren:</p>";
+  html += "<p align='center'><a href='https://www.buymeacoffee.com/mallewski' target='_blank'><img src='https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png' alt='Buy Me A Coffee' height='60' width='217'></a></p>";
 
   html += "</div>"; // settingsTab
   return html;
