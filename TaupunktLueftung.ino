d958ca25f944a3ceaf31242115f509457d32b85d
mallewski <m.ellekotten@posteo.de>
2025-04-30 13:59:12 +0200

    Lueftertimer ergänzt

--- TaupunktLueftung.ino
+++ TaupunktLueftung.ino
@@ -977,7 +977,7 @@ String getSettingsHtml() {
   html += "Mindestlaufzeit (Minuten): <input type='number' name='laufzeit' value='" + String(mindestLaufzeit_ms / 60000) + "' "
           "title='Mindestzeit, die die Lüftung nach dem Einschalten aktiv bleiben muss.\n"
           "Verhindert zu schnelles Ausschalten und schützt das Relais vor häufigem Schalten.\n"
-          "Tipp: 2–5 Minuten (0 Minuten = deaktiviert).'><br>";
+          "Tipp: 1–2 Minuten (0 Minuten = deaktiviert).'><br>";
   html += "Mindestpause (Minuten): <input type='number' name='pause' value='" + String(mindestPause_ms / 60000) + "' "
           "title='Mindestwartezeit nach dem Ausschalten, bevor die Lüftung erneut aktiviert werden darf.\n"
           "Dient dem Geräteschutz und reduziert unwirksames Lüften im Grenzbereich.\n"
